e3a772db9fffb4dbad9036d5b2d60cbf363f490b
SAFIDISOA <safidylegrand@gmail.com>
2025-06-25 12:59:59 +0300

    H

--- header.php
+++ header.php
@@ -1,13 +1,50 @@
 <!DOCTYPE html>
+
+
+
+<meta charset="utf-8">
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    session_start();
+    require "./files/database.php";
+    require "./files/functions.php";
+?>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Gestion Absence</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/font-awesome.min.css">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css" >
+    <link rel="stylesheet" href="./css/font-awesome.min.css" >
+    <link rel="stylesheet"  href="./css/styles.css" >
 
     <style>
+        nav .container{
+            display: flex;
+            justify-content: space-between;
+        }
+
+        #deconect {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+            background-color: #999999;
+            padding: 15px;
+            margin-top: 5px;
+            border-radius: 10px;
+            color: white;
+        }
+
+        #deconect:hover {
+            background-color: gray;
+        }
+
+        .nom {
+            background-color: orange;
+            border-radius: 5px;
+        }
+
         body.light-mode {
             background-color: #ffffff;
             color: #000000;
@@ -32,79 +69,62 @@
 
         #theme-toggle {
             margin-left: 10px;
-        }
-
-        .badge-custom {
-            background-color: #ffc107;
-            color: #212529;
-            padding: 5px 10px;
-            border-radius: 5px;
-            font-weight: bold;
+            margin-top: 10px;
         }
     </style>
 </head>
-
-<?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    session_start();
-    require "./files/database.php";
-    require "./files/functions.php";
-?>
-
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="padding: 12px 0;">
-  <div class="container">
-    <!-- Logo -->
-    <a class="navbar-brand" href="index.php" style="font-weight: bold; font-size: 28px;">
-      <i class="fa fa-graduation-cap"></i> Ny <span class="badge-custom">Sekoliko</span>
-    </a>
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color: black;font-size:  25px;">
+    <div class="container">
+        <!-- Brand -->
+        <div class="navbar-header">   
+            <a class="navbar-brand" href="index.php" style="font-weight: 700;font-size:30px;padding:5px 0 0 0;">
+                <span style="color: white;">Ny  </span><span style="color: black;" class="nom"> Sekoliko</span>
+            </a>
+        </div>
 
-    <!-- Bouton responsive -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+        <!-- Liens -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Accueil</a></li>
+                <?php if(isset($_SESSION["login"]) && $_SESSION["type"] == "admin" ): ?>
+                    <li><a href="index.php"> <i class="fa fa-dashboard"></i> <b>ADMIN</b></a></li>
+                <?php elseif(isset($_SESSION["type"]) && $_SESSION["type"] == "etudiant" ): ?>
+                    <li><a href="etudiant.php?id=<?= $_SESSION['id_etudiant'] ?>"> <i class="fa fa-dashboard"></i> Mes absences</a></li>
+                <?php elseif(isset($_SESSION["type"]) && $_SESSION["type"] == "professeur" ): ?>
+                    <li><a href="index.php"> <i class="fa fa-dashboard"></i> <?= $_SESSION["nom"] ?></a></li>
+                <?php endif; ?>
 
-    <!-- Menu -->
-    <div class="collapse navbar-collapse" id="navbarContent">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
-        <?php if(isset($_SESSION["login"]) && $_SESSION["type"] == "admin" ): ?>
-            <li class="nav-item"><a class="nav-link" href="index.php"><i class="fa fa-dashboard"></i> <b>ADMIN</b></a></li>
-        <?php elseif(isset($_SESSION["type"]) && $_SESSION["type"] == "etudiant" ): ?>
-            <li class="nav-item"><a class="nav-link" href="etudiant.php?id=<?= $_SESSION['id_etudiant'] ?>"><i class="fa fa-dashboard"></i> Mes absences</a></li>
-        <?php elseif(isset($_SESSION["type"]) && $_SESSION["type"] == "professeur" ): ?>
-            <li class="nav-item"><a class="nav-link" href="index.php"><i class="fa fa-dashboard"></i> <?= $_SESSION["nom"] ?></a></li>
-        <?php endif; ?>
+                <?php if(!isset($_SESSION["login"])): ?>
+                    <li><a href="signup.php">Inscription</a></li>
+                    <li><a href="login.php">Connexion</a></li>
+                <?php endif ?>
+            </ul>
+        </div>
 
-        <?php if(!isset($_SESSION["login"])): ?>
-            <li class="nav-item"><a class="nav-link" href="signup.php">Inscription</a></li>
-            <li class="nav-item"><a class="nav-link" href="login.php">Connexion</a></li>
-        <?php endif ?>
-      </ul>
-
-      <!-- Partie droite -->
-      <div class="d-flex align-items-center">
-        <?php if(isset($_SESSION["id"])): ?>
-          <a href="disconnect.php" class="btn btn-outline-light me-2" id="deconect"><i class="fa fa-sign-out"></i> Déconnexion</a>
-        <?php endif ?>
-        <button id="theme-toggle" class="btn btn-light">🌓</button>
-      </div>
+        <!-- Partie droite : Déconnexion + Thème -->
+        <div style="display: flex; align-items: center;">
+            <?php if(isset($_SESSION["id"])): ?>
+                <a href="disconnect.php" id="deconect"><i class="fa fa-sign-out"></i> Se déconnecter</a>
+            <?php endif ?>
+            <button id="theme-toggle" class="btn btn-sm btn-default">🌓 Thème</button>
+        </div>
     </div>
-  </div>
 </nav>
 
 <!-- Message flash -->
 <?php if(isset($_SESSION["message"])): ?>
-  <div class="container mt-3">
-    <div class="alert alert-info">
-      <?php echo $_SESSION["message"] ?>
+    <div class="container">
+        <div class="row">
+            <div class="alert alert-info">
+                <?php echo $_SESSION["message"] ?>
+            </div>
+        </div>
     </div>
-  </div>
-<?php endif; ?>
+<?php endif ?>
 
-<!-- JS thème sombre/clair -->
+<!-- JS pour thème -->
 <script>
   window.onload = function () {
     const savedTheme = localStorage.getItem("theme") || "light";
